7dff9c6e96fa8a81719293a42a2af162f39ed1f3
nik-hz <nik0001@protonmail.com>
2025-09-10 18:22:44 -0400

    /

---
+++ generator_file.py
@@ -0,0 +1,21 @@
+from Scarlet.genBenchmarks import SampleGenerator
+from Scarlet.ltllearner import LTLlearner
+import os, glob
+
+OUT = "bench_out"
+SampleGenerator(
+    formula_file="formulas_scarlet.txt",
+    sample_sizes=[(10, 10)],  # pos,neg per formula
+    trace_lengths=[(12, 48)],  # min,max length
+    output_folder=OUT,
+).generate()
+
+
+traces = sorted(glob.glob(os.path.join(OUT, "*.trace")))
+assert traces, "No .trace files generated"
+print("Using trace:", traces[0])
+
+learner = LTLlearner(
+    input_file=traces[0], timeout=60, thres=0, csvname=os.path.join(OUT, "learn.csv")
+)
+print("Learned formula:", learner.learn())
